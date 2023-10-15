5b7799e716bbdaaa28d71539d56dbb40144e5992
Andy Lang <djdreviz@gmail.com>
2023-10-16 00:11:37 +0300

    6. Using Variables done!

---
+++ Section_2/_06._Using_Variables/src/Application.java
@@ -0,0 +1,8 @@
+// 6. Using Variables
+public class Application {
+    public static void main(String[] args) {
+        int number;
+        number = 88;
+        System.out.println(number);
+    }
+}
