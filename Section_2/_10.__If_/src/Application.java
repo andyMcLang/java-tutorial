61aa8ee22a9fff31c77243588ae9bf992f7ba2cf
Andy Lang <djdreviz@gmail.com>
2023-10-16 01:00:18 +0300

    10. If done!

---
+++ Section_2/_10.__If_/src/Application.java
@@ -0,0 +1,17 @@
+//10. "If"
+public class Application {
+    public static void main(String[] args) {
+        int arvo = 0;
+
+        while (true) {
+
+            if (arvo == 5) {
+                break;
+            }
+
+            arvo++;
+
+            System.out.println("Käy..");
+        }
+    }
+}
