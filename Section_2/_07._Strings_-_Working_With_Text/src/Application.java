d2c4a77dc6f329895f5647893f9d1559f9c04c2a
Andy Lang <djdreviz@gmail.com>
2023-10-16 00:23:47 +0300

    7. Strings: Working With Text done!

---
+++ Section_2/_07._Strings_-_Working_With_Text/src/Application.java
@@ -0,0 +1,13 @@
+// 7. Strings: Working With Text
+public class Application {
+    public static void main(String[] args) {
+        int number = 7;
+        String teksti = "Terve";
+        String tyhja = " ";
+        String nimi = "Andy";
+        String tervehdys = teksti + tyhja + nimi;
+
+        System.out.println(tervehdys);
+        System.out.println(number + " on numeroni.");
+    }
+}
