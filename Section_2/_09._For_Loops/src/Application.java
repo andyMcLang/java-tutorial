d417913cad7cec1d60cbd50dfad74fe851c1991e
Andy Lang <djdreviz@gmail.com>
2023-10-16 00:48:08 +0300

    9. For Loops done!

---
+++ Section_2/_09._For_Loops/src/Application.java
@@ -0,0 +1,10 @@
+// 9. For Loops
+public class Application {
+    public static void main(String[] args) {
+        int arvo = 10;
+
+        for (int i = 1; i <= arvo; i++) {
+            System.out.printf("Arvo i on: %d\n", i);
+        }
+    }
+}
