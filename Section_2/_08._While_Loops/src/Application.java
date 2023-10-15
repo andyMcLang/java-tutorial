3787ff5bf0a3badff0c538e7a5bf59433c28bbfb
Andy Lang <djdreviz@gmail.com>
2023-10-16 00:31:36 +0300

    8. While Loops done!

---
+++ Section_2/_08._While_Loops/src/Application.java
@@ -0,0 +1,11 @@
+// 8. While Loops
+public class Application {
+    public static void main(String[] args) {
+        int arvo = 0;
+
+        while (arvo < 10) {
+            System.out.println(arvo);
+            arvo++;
+        }
+    }
+}
