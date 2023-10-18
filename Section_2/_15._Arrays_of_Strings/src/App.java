2a20b715d44c54e6f9d05ade9d873ecf5ca19000
Andy Lang <djdreviz@gmail.com>
2023-10-18 16:08:32 +0300

    15. Arrays of Strings done!

---
+++ Section_2/_15._Arrays_of_Strings/src/App.java
@@ -0,0 +1,11 @@
+// 15. Arrays of Strings
+public class App {
+    public static void main(String[] args) {
+        String[] tekstia = new String[3];
+        tekstia[0] = "Terve";
+
+        for (String teksti : tekstia) {
+            System.out.println(teksti);
+        }
+    }
+}
